c070af46ef60aa6a4eb4bbd17e2f7cdfd6de9f26
ryanzola <ryanzola@me.com>
2023-12-01 21:28:41 -0500

    :book: update readme

--- js/app.js
+++ js/app.js
@@ -1,42 +1,36 @@
-import { PerspectiveCamera, Scene, WebGLRenderer } from 'three';
-
-import Particles from './particles';
-import Tube from './tube';
-import Caustics from './caustics';
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
 export default class Sketch {
   constructor(options) {
     this.container = options.dom;
-    this.scene = new Scene();
+    this.scene = new THREE.Scene();
 
     this.width = this.container.offsetWidth;
     this.height = this.container.offsetHeight;
 
-    this.renderer = new WebGLRenderer({
+    this.renderer = new THREE.WebGLRenderer({
       antialias: true,
       alpha: true
     });
     this.renderer.setSize(this.width, this.height);
     this.renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
-    this.renderer.setClearColor(0x05233c, 1);
+    this.renderer.setClearColor(0x222222, 1);
     this.container.appendChild(this.renderer.domElement);
 
-    this.camera = new PerspectiveCamera(70, this.width / this.height, 0.01, 1000);
-    this.camera.position.z = 4;
-    this.initZ = this.camera.position.z;
-    this.cameraParams = {
-      intensity: 0.001,
-      ease: 0.08
-    }
+    this.camera = new THREE.PerspectiveCamera(70, this.width / this.height, 0.01, 1000);
+    this.camera.position.z = 2;
+
+    this.controls = new OrbitControls(this.camera, this.renderer.domElement);
+    // smooth controls
+    this.controls.enableDamping = true;
 
     this.mouse = { x: 0, y: 0 };
 
     this.setupResize();
     this.resize();
     this.mouseEvents();
-    this.addParticles();
-    this.addTube();
-    this.addCaustics();
+    this.addObjects();
     this.render()
   }
 
@@ -57,40 +51,22 @@ export default class Sketch {
 
   mouseEvents() {
     window.addEventListener('mousemove', (e) => {
-      this.mouse.x = (e.clientX - window.innerWidth / 2) * this.cameraParams.intensity;
-      this.mouse.y = (e.clientY - window.innerHeight / 2) * this.cameraParams.intensity;
+      this.mouse.x = (e.clientX - window.innerWidth / 2);
+      this.mouse.y = (e.clientY - window.innerHeight / 2);
     });
   }
 
-  addParticles() {
-    this.particles = new Particles({
-      scene: this.scene
-    })
+  addObjects() {
+    this.geometry = new THREE.PlaneGeometry(1, 1, 1, 1);
+    this.material = new THREE.MeshNormalMaterial();
+
+    this.mesh = new THREE.Mesh(this.geometry, this.material);
+    this.scene.add(this.mesh);
   }
 
-  addTube() {
-    this.tube = new Tube({
-      scene: this.scene
-    })
-  }
-
-  addCaustics() {
-    this.caustics = new Caustics({
-      scene: this.scene
-    })
-  }
 
   render() {
-    this.camera.position.x += (this.mouse.x - this.camera.position.x) * this.cameraParams.ease;
-    this.camera.position.y += (this.mouse.y - this.camera.position.y) * this.cameraParams.ease;
-    this.camera.position.z += (this.initZ - this.camera.position.z) * this.cameraParams.ease;
-    this.camera.lookAt(0, 0, 0);
-
-    if (this.particles) this.particles.update();
-
-    if (this.tube) this.tube.update();
-
-    if (this.caustics) this.caustics.update();
+    this.controls.update();
 
     this.renderer.render(this.scene, this.camera);
 
--- js/shader/caustics/vertex.glsl
+++
@@ -1,9 +0,0 @@
-varying vec3 vWorldPosition;
-varying vec2 vUv;
-
-void main() {
-  vUv = uv;
-  vWorldPosition = (modelMatrix * vec4( position, 1.0 )).xyz;
-
-  gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
-}
